c3be4d7bae4c9bc95ed61d31d044eeda1fbbd4c4
ahmouda12 <ahmouda12@ufl.edu>
2018-09-13 20:05:33 -0400

    Deploying to Heroku

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const passport     = require('passport');
 var cors           = require('cors');
 
 
+
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/twitter-explorer', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
